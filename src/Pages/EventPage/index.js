b3f429bb80b24400b227e90d00335f835b3a46f5
Cuma Umur Doğan <umurdogann@gmail.com>
2022-01-07 05:59:27 +0300

    Update event page to pass data to components

--- src/Pages/EventPage/index.js
+++ src/Pages/EventPage/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EventList from '../../Components/EventList';
 import EventDetails from '../../Components/EventDetails';
 import { Container, Header, SideContainer, Wrapper } from './styled';
 
-const EventPage = () => {
+const EventPage = ({ responseData }) => {
+	const [selectedEvent, setSelectedEvent] = useState(-1);
+	const [data, setData] = useState(responseData);
+
 	return (
 		<Container>
 			<Wrapper>
 				<SideContainer className='list-container'>
 					<Header className='list-header'>EVENTS</Header>
-					<EventList />
+					<EventList data={data} selected={selectedEvent} setSelected={setSelectedEvent} />
 				</SideContainer>
-				<SideContainer className='details-container'>
-					<Header>EVENT DETAILS</Header>
-					<EventDetails />
-				</SideContainer>
+				{selectedEvent !== -1 && (
+					<SideContainer className='details-container'>
+						<Header>EVENT DETAILS</Header>
+						<EventDetails data={data} index={selectedEvent} setData={setData}/>
+					</SideContainer>
+				)}
 			</Wrapper>
 		</Container>
 	);
