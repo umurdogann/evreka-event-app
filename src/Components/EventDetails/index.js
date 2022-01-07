f5d44d7510d1569a99e3967110d6e24ed13f427a
Cuma Umur Doğan <umurdogann@gmail.com>
2022-01-07 06:10:26 +0300

    Add functionality that hides the buttons when the event action is empty and passes the selected event data from the EventDetails component to its child components.

--- src/Components/EventDetails/index.js
+++ src/Components/EventDetails/index.js
@@ -3,13 +3,21 @@ import { ButtonContainer, Container } from './styled';
 import Button from '../Button/index';
 import Tabs from '../Tabs';
 import Modal from '../Modal';
+import { useEffect } from 'react/cjs/react.development';
 
-const EventDetails = () => {
+const EventDetails = ({ index, data, setData }) => {
 	const [showModal, setShowModal] = useState(false);
 	const [showButtons, setShowButtons] = useState(true);
 
+	useEffect(() => {
+		setShowButtons(data[index].details[4].value === '-');
+	}, [data, data.details, index]);
+
 	const handleClick = () => {
 		setShowButtons(false);
+		let temp = [...data];
+		temp[index].details[4].value = 'No Action Needed';
+		setData(temp);
 	};
 
 	return (
@@ -18,16 +26,24 @@ const EventDetails = () => {
 				<ButtonContainer>
 					<Button className='secondary' title='NO ACTION NEEDED' onClick={handleClick} />
 					<Button className='primary' title='TAKE ACTION' onClick={() => setShowModal(true)} />
-					{showModal && (
-						<Modal
-							show={showModal}
-							setShow={() => setShowModal(false)}
-							content={<Tabs type='action' closeCallback={() => setShowModal(false)} />}
-						/>
-					)}
 				</ButtonContainer>
 			)}
-			<Tabs type={'eventDetails'} />
+			<Tabs type={'eventDetails'} data={data[index]} />
+			{showModal && (
+				<Modal
+					show={showModal}
+					setShow={() => setShowModal(false)}
+					content={
+						<Tabs
+							type='action'
+							index={index}
+							data={data}
+							setData={setData}
+							closeCallback={() => setShowModal(false)}
+						/>
+					}
+				/>
+			)}
 		</Container>
 	);
 };
